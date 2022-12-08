54cb80a45464eb9ebbaeac25129cdd0922d72611
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2022-12-08 20:40:47 +0300

    images

---
+++ images/main.js
@@ -0,0 +1,14 @@
+function validate()
+{
+    var username=document.getElementById("username").Value;
+    var password=document.getElementById("password").Value;
+    if (username=='Admin' && password='user')
+    {
+        alert('login succesfully');
+        return failse;
+    }
+    else
+    {
+       alert('login')
+    }
+}
